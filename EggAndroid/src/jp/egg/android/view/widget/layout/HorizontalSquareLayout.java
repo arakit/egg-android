2abf75c7a5571e690e598fb26c0be194ab321f6d
Chikara Funabashi <hunatika615@gmail.com>
2014-10-21 23:36:20 +0900

    Mod HorizontalSquare

--- EggAndroid/src/jp/egg/android/view/widget/layout/HorizontalSquareLayout.java
+++ EggAndroid/src/jp/egg/android/view/widget/layout/HorizontalSquareLayout.java
@@ -1,32 +1,50 @@
 package jp.egg.android.view.widget.layout;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
+import jp.egg.android.R;
 
 /**
  * Created by chikara on 2014/09/11.
  */
 public class HorizontalSquareLayout extends FrameLayout{
 
+    private float mVerticalWeight;
+
     public HorizontalSquareLayout(Context context) {
         super(context);
+        init(context, null);
     }
 
     public HorizontalSquareLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public HorizontalSquareLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        init(context, attrs);
     }
 
 
+    private void init (Context context, AttributeSet attrs){
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SquareLayout);
+        mVerticalWeight = a.getFloat(R.styleable.SquareLayout_slWeight, 1.0f);
+    }
 
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+        int measureWidthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int measureWidthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+
+        super.onMeasure(
+                widthMeasureSpec,
+                MeasureSpec.makeMeasureSpec((int)(measureWidthSize * mVerticalWeight), measureWidthMode)
+        );
     }
 
 }
