f6951dcf06e736a8e08c46b34c2826cdaf97d50f
Chikara Funabashi <hunatika615@gmail.com>
2014-11-04 04:29:57 +0900

    FileDownloadTask.

---
+++ EggAndroid/src/jp/egg/android/util/Tupple1.java
@@ -0,0 +1,11 @@
+package jp.egg.android.util;
+
+public class Tupple1<A>{
+
+    public A i1;
+
+    public Tupple1(A a) {
+        i1 = a;
+    }
+
+}
--- EggAndroid/src/jp/egg/android/util/Tupple2.java
+++ EggAndroid/src/jp/egg/android/util/Tupple2.java
@@ -2,12 +2,12 @@ package jp.egg.android.util;
 
 public class Tupple2 <A,B>{
 
-	public A i1;
-	public B i2;
+    public A i1;
+    public B i2;
 
-	public Tupple2(A a, B b) {
-		i1 = a;
-		i2 = b;
-	}
+    public Tupple2(A a, B b) {
+        i1 = a;
+        i2 = b;
+    }
 
 }
