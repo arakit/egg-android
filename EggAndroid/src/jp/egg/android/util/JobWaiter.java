31781b2134c730363c26c0be5fb8da5141ab0b74
Chikara Funabashi <hunatika615@gmail.com>
2017-12-13 17:06:04 +0900

    Add jobwaiter is emoty method.

--- EggAndroid/src/jp/egg/android/util/JobWaiter.java
+++ EggAndroid/src/jp/egg/android/util/JobWaiter.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * Created by chikara on 2015/01/12.
  */
-public class JobWaiter {
+public final class JobWaiter {
 
     public static final String TAG = "JobWaiter";
 
@@ -16,20 +16,20 @@ public class JobWaiter {
 
     }
 
-    public synchronized Object job() {
+    public final synchronized Object job() {
         Object job = new Object();
         jobs.add(job);
         Log.d(TAG, "job start " + job);
         return job;
     }
 
-    public synchronized void complete(Object job) {
+    public final synchronized void complete(Object job) {
         jobs.remove(job);
         Log.d(TAG, "job end " + job);
         this.notifyAll();
     }
 
-    public synchronized void waitForCompleteAll() {
+    public final synchronized void waitForCompleteAll() {
         while (jobs.size() > 0) {
             try {
                 JobWaiter.this.wait();
@@ -40,7 +40,11 @@ public class JobWaiter {
         }
     }
 
-    public void listenCompleteAll(final Runnable callback) {
+    public final synchronized boolean isEmpty() {
+        return jobs.isEmpty();
+    }
+
+    public final void listenCompleteAll(final Runnable callback) {
         HandlerUtil.postBackground(new Runnable() {
             @Override
             public void run() {
